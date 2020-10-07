121341e44dd7904a7c85e45da64187e2b98036a4
Christian Hodges <cdhodges5@gmail.com>
2020-10-07 13:04:42 -0700

    these files weren't deleted when they should've been on the pull request

--- whattowatch/watch/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import UserM
-from django import forms
-
-class UserF(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    class Meta:
-        model = UserM
-        fields = ('username', 'email', 'password')
