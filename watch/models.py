c6090b43da374b8a993993b67cc95f4489b4b7d8
Christian Hodges <cdhodges5@gmail.com>
2020-10-12 18:28:19 -0700

    Work for 10/12

--- watch/models.py
+++ watch/models.py
@@ -2,9 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserM(models.Model):
-    username = models.CharField(max_length=120)
-    email = models.CharField(max_length=120)
-    password = models.CharField(max_length=120)
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
 
     def _str_(self):
         return self.username
+
+class Profile(models.Model):
+    profile_fname = models.CharField(max_length=50)
+    profile_lname = models.CharField(max_length=50)
+    profile_bio = models.CharField(max_length=500)
+    profile_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        name = self.profile_fname + " " + profile_lname
+        return name
--- watch/urls.py
+++ watch/urls.py
@@ -1,11 +1,15 @@
-from . import views
-from watch.views import home
 from django.contrib.auth import authenticate
-from . import views
+from django.contrib.auth import views as auth_views
 from django.urls import path, re_path
+
+from . import views
+
+
 urlpatterns = [
-        path('', home, name='home'),
+        path('', views.blank, name='blank'),
         path('home/', views.home),
-        path('login_view/', views.login_view),
+        path('login/', views.login_view),
         path('signup/', views.signup, name="signup"),
+        path('logout/', views.logout_view, name="logout"),
+        path('profilePage/', views.profile_view, name="profile"),
 ]
