0e9892efde27e54864bb31c869fc29a5db472435
Christian Hodges <cdhodges5@gmail.com>
2020-10-20 13:30:16 -0700

    Profile pictures actually working now. Resolves #4

--- watch/urls.py
+++ watch/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth import authenticate
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 from django.urls import path, re_path
 
 from . import views
@@ -12,4 +14,4 @@ urlpatterns = [
         path('signup/', views.signup, name="signup"),
         path('logout/', views.logout_view, name="logout"),
         path('profilePage/', views.profile_view, name="profile"),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
