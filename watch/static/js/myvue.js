615d94e1dd3cb506ead5c6594981ca7abfaac2d7
Christian Hodges <cdhodges5@gmail.com>
2020-11-02 12:35:06 -0800

    Updated base.html and got rid of autoupdate for /friends

--- watch/static/js/myvue.js
+++ watch/static/js/myvue.js
@@ -1,33 +1,8 @@
 var app3 = new Vue({
 	el: '#app-3',
 	data: {
-		show: false 
+		show: false
 	}
-})
-
-var app4 = new Vue({
-	el: '#app-4',
-	data: {
-		pets: [],
-	},
-	//Adapted from https://stackoverflow.com/questions/36572540/vue-js-auto-reload-refresh-data-with-timer
-	created: function() {
-		this.fetchPetList();
-		this.timer = setInterval(this.fetchPetList, 30000);
-	},
-	methods: {
-		fetchPetList: function() {
-			axios
-				.get('/pets/')
-				.then(response => (this.pets = response.data.pets))
-			console.log(this.pets)
-		},
-		cancelAutoUpdate: function() { clearInterval(this.timer) }
-	},
-	beforeDestroy() {
-		clearInterval(this.timer)
-	}
-
 })
 
 var app5 = new Vue({
