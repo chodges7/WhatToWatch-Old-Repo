40e20cf674c13e5390396ee9a31deea743a55277
vdhaka <vdhaka@mail.csuchico.edu>
2020-11-04 20:48:57 +0000

    Hosting Complete

---
+++ static/js/myvue.js
@@ -0,0 +1,63 @@
+var app3 = new Vue({
+	el: '#app-3',
+	data: {
+		show: false
+	}
+})
+
+var app3 = new Vue({
+	el: '#app-2',
+	data: {
+		show: false
+	}
+})
+
+var app5 = new Vue({
+	el: '#app-5',
+	data: {
+		friends: [],
+	},
+	//Adapted from https://stackoverflow.com/questions/36572540/vue-js-auto-reload-refresh-data-with-timer
+	created: function() {
+		this.fetchFriendList();
+		this.timer = setInterval(this.fetchFriendList, 30000);
+	},
+	methods: {
+		fetchFriendList: function() {
+			axios
+				.get('/friends/')
+				.then(response => (this.friends = response.data.friends))
+			console.log(this.friends)
+		},
+		cancelAutoUpdate: function() { clearInterval(this.timer) }
+	},
+	beforeDestroy() {
+		clearInterval(this.timer)
+	}
+
+})
+
+var app6 = new Vue({
+	el: '#app-6',
+	data: {
+		statuses: [],
+	},
+	//Adapted from https://stackoverflow.com/questions/36572540/vue-js-auto-reload-refresh-data-with-timer
+	created: function() {
+		this.fetchStatusList();
+		this.timer = setInterval(this.fetchStatusList, 30000);
+	},
+	methods: {
+		fetchStatusList: function() {
+			axios
+				.get('/statuses/')
+				.then(response => (this.statuses = response.data.statuses))
+			console.log(this.friends)
+		},
+		cancelAutoUpdate: function() { clearInterval(this.timer) }
+	},
+	beforeDestroy() {
+		clearInterval(this.timer)
+	}
+
+})
