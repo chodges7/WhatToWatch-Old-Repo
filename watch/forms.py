14052c9d4b9343e4f2a46386b488f90c1f6dc733
Christian Hodges <cdhodges5@gmail.com>
2020-11-30 13:16:16 -0800

    Made a search bar form

--- watch/forms.py
+++ watch/forms.py
@@ -9,6 +9,9 @@ class ProfileForm(forms.Form):
     profile_fname = forms.CharField(label='Your corrected first name', max_length=50)
     profile_lname = forms.CharField(label='Your corrected last name', max_length=50)
 
+class SearchForm(forms.Form):
+    search_field = forms.CharField(label='Search any movie', max_length=50)
+
 class BioForm(forms.Form):
     profile_bio = forms.CharField(label='Your new Bio', max_length=500)
 
