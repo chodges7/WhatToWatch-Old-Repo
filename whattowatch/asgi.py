a5f24d77890e391f0fe35ead2742c7e40730a770
Christian Hodges <cdhodges5@gmail.com>
2020-12-02 13:17:59 -0800

    I don't know which of these two codes need to be here.

--- whattowatch/asgi.py
+++ whattowatch/asgi.py
@@ -6,3 +6,12 @@ from django.core.asgi import get_asgi_application
 import watch.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whattowatch.settings")
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            watch.routing.websocket_urlpatterns
+        )
+    ),
+})
