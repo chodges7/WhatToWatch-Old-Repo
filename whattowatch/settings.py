0987b3ec1e483496880af0552c6da0ca2ca4036b
Christian Hodges <cdhodges5@gmail.com>
2021-01-03 12:24:54 -0800

    Use python-dotenv to hide django secret key

--- whattowatch/settings.py
+++ whattowatch/settings.py
@@ -6,7 +6,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -17,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k!^)9(3r^b)r!mlq91wtqa=e4visjd_7qf1z$@kevc@*@@(ui4'
+load_dotenv()
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
